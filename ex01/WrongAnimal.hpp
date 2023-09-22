be694e5b04afe005ca2b97b70ae084521352a162
hikaru2003 <hikaru.morisaki.0316@gmail.com>
2023-09-22 10:56:50 +0900

    WrongAnimal.hpp makeSoundのvirtualを外した

--- ex01/WrongAnimal.hpp
+++ ex01/WrongAnimal.hpp
@@ -6,7 +6,7 @@
 /*   By: hikaru <hikaru@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 13:09:55 by hikaru            #+#    #+#             */
-/*   Updated: 2023/09/03 11:48:53 by hikaru           ###   ########.fr       */
+/*   Updated: 2023/09/22 10:52:56 by hikaru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,5 @@ public:
 	WrongAnimal &operator=(const WrongAnimal &animal);
 
 	std::string		getType() const;
-	virtual void	makeSound() const;
+	void	makeSound() const;
 };
